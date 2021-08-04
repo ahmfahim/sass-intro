f65e3b76cdba6528bbba689431cc4738d188f3b9
ahmfahim <fahimmorshed2019@gmail.com>
2021-08-04 19:55:49 +0600

    sass

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="first-box">
+                <h1>1st container</h1>
+            </div>
+            <div className="second-box">
+                <h1>2nd container</h1>
+            </div>
+            <div className="third-box">
+                <h1>3rd container</h1>
+            </div>
+            <div className="fourth-box">
+                <h1>4rt container</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
